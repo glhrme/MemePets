ed162a759d1332b082c9ba74f439844027def5e3
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-04-22 15:26:27 -0300

    adiciona activity FormAnimal e adiciona click no botao FAB para levar ao form

--- app/src/main/java/br/com/guisantos/datastorage/LauncherActivity.kt
+++ app/src/main/java/br/com/guisantos/datastorage/LauncherActivity.kt
@@ -1,14 +1,19 @@
 package br.com.guisantos.datastorage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import br.com.guisantos.datastorage.activities.FormAnimal
 import br.com.guisantos.datastorage.adapters.AnimalAdapterView
 import br.com.guisantos.datastorage.database.AnimalDatabase
 import br.com.guisantos.datastorage.database.dao.AnimalDao
 import br.com.guisantos.datastorage.database.entities.Animal
 import br.com.guisantos.datastorage.views.AnimalView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class LauncherActivity : AppCompatActivity() {
     private var database: AnimalDatabase? = null
@@ -20,11 +25,20 @@ class LauncherActivity : AppCompatActivity() {
         setContentView(R.layout.activity_launcher)
         database = AnimalDatabase.getInstance(this)
         dao = database?.getAnimalDao()
+        fabAddAnimal()
     }
 
     override fun onResume() {
         super.onResume()
         val recyclerView: RecyclerView = findViewById(R.id.ac_launcher_animal_list)
         AnimalView(dao!!.getAll(), recyclerView, this).init()
+    }
+
+    fun fabAddAnimal() {
+        findViewById<FloatingActionButton>(R.id.ac_launcher_btn_adicionar).setOnClickListener(
+                View.OnClickListener() {
+                    view: View -> startActivity(Intent(this, FormAnimal::class.java))
+                }
+        )
     }
 }
---
+++ app/src/main/java/br/com/guisantos/datastorage/activities/FormAnimal.kt
@@ -0,0 +1,12 @@
+package br.com.guisantos.datastorage.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.guisantos.datastorage.R
+
+class FormAnimal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_animal)
+    }
+}
