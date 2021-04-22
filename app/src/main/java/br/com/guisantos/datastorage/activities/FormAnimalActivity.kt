82230b81ee1147c50fd759a018153ecb3f84b039
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-04-22 20:19:35 -0300

    adiciona formulário de adição dos animais e menus na appbar

--- app/src/main/java/br/com/guisantos/datastorage/activities/FormAnimalActivity.kt
+++ app/src/main/java/br/com/guisantos/datastorage/activities/FormAnimalActivity.kt
@@ -2,12 +2,30 @@ package br.com.guisantos.datastorage.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import br.com.guisantos.datastorage.R
+
 
 class FormAnimalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_animal)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.ac_form_menu_options, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.ac_form_menu_cancel) {
+            finish()
+        } else {
+            Toast.makeText(this, "Salvar", Toast.LENGTH_LONG).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun finish() {
